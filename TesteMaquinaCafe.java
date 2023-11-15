1c5a8f9feee8f58435da683f2868f26ce8ca7b85
Felipe Perlato <fe.perlato@gmail.com>
2023-11-15 19:55:58 -0300

    primeiro commit

---
+++ TesteMaquinaCafe.java
@@ -0,0 +1,13 @@
+package MaquinaCafé;
+
+public class TesteMaquinaCafe {
+	public static void main(String[] args) {
+		MaquinaCafe maquina = new MaquinaCafe();
+		maquina.acucarDisponivel = 30;
+		
+		maquina.fazerCafe(10);
+		maquina.fazerCafe(15);
+		maquina.fazerCafe(10);
+		
+	}
+}
